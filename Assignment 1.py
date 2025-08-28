25a5a37eab2fb8062a295775d86a25b96fac8376
saijanani4 <141271291+saijanani4@users.noreply.github.com>
2025-08-28 15:28:20 -0400

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,17 @@
+x=input("Enter the first number:")
+y=input("Enter the second number:")
+
+x=float(x)
+y=float(y)
+
+print("Addition: ",x+y)
+print("Subtraction: ", x-y)
+print("Multiplication: ", x*y)
+print("Division: ", x/y)
+
+#----------------------------------------
+a=input("Enter the first name:")
+b=input("Enter the last name:")
+
+print("Hello, "+a+" "+b+"! Welcome to the program.")
+
